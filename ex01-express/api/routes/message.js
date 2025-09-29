b978da401728d7d5cb227f7444883ce26d267a01
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-09-29 19:45:30 -0300

    .

--- ex01-express/api/routes/message.js
+++ ex01-express/api/routes/message.js
@@ -1,31 +1,74 @@
 import { Router } from "express";
-import messageController from "../controllers/messageController";
-import { isAuthenticated, isResourceOwner } from "../middleware/authMiddleware";
+import models from "../models/index.js";
 
 const router = Router();
 
-router.get("/", messageController.getAllMessages);
+router.get("/", async (req, res) => {
+  let { userId } = req.query;
 
-router.get("/:messageId", messageController.getMessageById);
+  let messages = await models.Message.findAll({
+    where: {
+      userId: userId,
+    },
+  });
+  return res.status(200).send(messages);
+});
 
-router.post(
-  "/",
-  isAuthenticated,
-  messageController.createMessage
-);
+router.get("/:messageId", async (req, res) => {
+  let id = req.params.messageId;
 
-router.put(
-  "/:messageId",
-  isAuthenticated,
-  isResourceOwner('Message'),
-  messageController.updateMessage
-);
+  let message = await models.Message.findOne({
+    where: {
+      id: id,
+    },
+  });
 
-router.delete(
-  "/:messageId",
-  isAuthenticated,
-  isResourceOwner('Message'),
-  messageController.deleteMessage
-);
+  if (message == null) return res.status(404).send("Mensagem não encontrada");
+
+  return res.status(200).send(message);
+});
+
+router.post("/", async (req, res) => {
+  let { userId } = req.query;
+
+  let message = await models.Message.create({
+    text: req.body.text,
+    userId: userId,
+  });
+
+  return res.status(201).send(message);
+});
+
+router.delete("/:messageId", async (req, res) => {
+  let id = req.params.messageId;
+
+  await models.Message.destroy({
+    where: {
+      id: id,
+    },
+  });
+
+  return res.status(204).send();
+});
+
+router.put("/:messageId", async (req, res) => {
+  let id = req.params.messageId;
+
+  let message = await models.Message.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (message == null) return res.status(404).send();
+
+  message.set({
+    text: req.body.text,
+  });
+
+  await message.save();
+
+  return res.status(200).send(message);
+});
 
 export default router;
