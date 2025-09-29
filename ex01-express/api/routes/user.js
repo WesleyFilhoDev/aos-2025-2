cd88d89cab324414da33afb5bc615c37c591974a
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-09-29 19:45:32 -0300

    commit

--- ex01-express/api/routes/user.js
+++ ex01-express/api/routes/user.js
@@ -1,27 +1,68 @@
 import { Router } from "express";
-import userController from "../controllers/userController";
-import { isAuthenticated, isResourceOwner } from "../middleware/authMiddleware";
+import models from "../models/index.js";
 
 const router = Router();
 
-router.get("/", userController.getAllUsers);
+router.get("/", async (req, res) => {
+  let users = await models.User.findAll();
 
-router.get("/:userId", userController.getUserById);
+  return res.status(200).send(users);
+});
 
-router.post("/", userController.createUser);
+router.get("/:userId", async (req, res) => {
+  let id = req.params.userId;
+  let user = await models.User.findOne({
+    where: {
+      id: id,
+    },
+  });
 
-router.put(
-  "/:userId",
-  isAuthenticated,
-  isResourceOwner('User'),
-  userController.updateUser
-);
+  if (user == null) return res.status(404).send("Usuário não localizado");
 
-router.delete(
-  "/:userId",
-  isAuthenticated,
-  isResourceOwner('User'),
-  userController.deleteUser
-);
+  return res.status(200).send(user);
+});
+
+router.post("/", async (req, res) => {
+  let data = req.body;
+
+  let newUser = await models.User.create(data);
+
+  return res.status(201).send(newUser);
+});
+
+router.put("/:userId", async (req, res) => {
+  let id = req.params.userId;
+
+  let user = await models.User.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (user == null) return res.status(404).send();
+
+  let { username, email } = req.body;
+
+  user.set({
+    username: username ? username : user.username,
+    email: email ? email : user.email,
+  });
+
+  await user.save();
+
+  return res.status(200).send(user);
+});
+
+router.delete("/:userId", async (req, res) => {
+  let id = req.params.userId;
+
+  await models.User.destroy({
+    where: {
+      id: id,
+    },
+  });
+
+  return res.status(204).send();
+});
 
 export default router;
